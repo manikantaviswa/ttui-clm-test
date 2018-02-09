3050251dc0c6e75f3072d70599383d4021cb9c5e
Manikanta S <manikanta.sathi@tecnotree.com>
2018-02-09 18:56:12 +0530

    test

--- Gruntfile.js
+++ Gruntfile.js
@@ -101,6 +101,80 @@ module.exports = function(grunt) {
 					extDot: 'last'
 				}]
 			}
+		},
+
+		ngtemplates: {
+			app: {
+				src:          'src/scripts/**/*.html',
+				dest:         moduleFilePrefix + '-views.js',
+				options:      {
+					bootstrap:  function(module, script) {
+						return 'define(' + module + ', [], function() { return { init: ' + script + ' }; });';
+					}
+				}
+			},
+			dist: {
+				options: {
+					src:      '**.html',
+				    dest:     'template.js',
+					module: function(src) {
+						var cwd = grunt.template.process('src/scripts/');
+						var modulePath = src.replace(cwd, '').split('/')[0];
+
+						console.log('Module Path', modulePath);
+
+						return modules[modulePath];
+					},
+					prefix: 'scripts',
+					bootstrap: function(moduleName, script) {
+
+						var header =
+							'/* commonjs package manager support (eg componentjs) */\n' +
+							'if (typeof module !== "undefined" && typeof exports !== "undefined" && module.exports === exports){\n' +
+							'	module.exports = \'@@@@__SOURCE_FILENAME__\';\n' +
+							'}\n\n' +
+							'(function (window, angular, undefined) {\n' +
+							'	"use strict";\n\n'+
+							'   angular.module(\''+moduleName+'\', []).run([\'$templateCache\', function($templateCache) {\n';
+
+						var footer =
+							'\n'+
+							'}]);\n' +
+							'return angular;\n' +
+							'})(window, window.angular);';
+
+						var cwd = grunt.template.process('app');
+						script = script.replace(new RegExp(cwd, 'g'), '');
+
+						script = script.replace(/(^|\n)[ \t]*('use strict'|"use strict");?\s*/g, '$1');
+
+						return grunt.template.process(header) + script + footer;
+					},
+					htmlmin: {
+						collapseBooleanAttributes: false,
+						collapseWhitespace: true,
+						removeAttributeQuotes: false,
+						removeComments: true,
+						removeEmptyAttributes: false,
+						removeRedundantAttributes: false,
+						removeScriptTypeAttributes: true,
+						removeStyleLinkTypeAttributes: true
+					}
+				},
+				files: [
+					{
+						expand: true,
+						cwd: 'app/scripts/',
+						src: [
+							'**/*.html'
+						],
+						dest: 'dist/scripts/',
+						rename: function(dest, src) {
+							return dest+moduleFilePrefix+src.split('/')[0]+'.tpl.js';
+						}
+					}
+				]
+			}
 		}
 	});
 
@@ -108,12 +182,14 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-angular-templates');
 
 	grunt.registerTask('build', [
 		'clean',
 		'copy:dist',
 		'concat:dist',
-		'concat:library'
+		'concat:library',
+		//'ngtemplates'
 //		'uglify'
 	]);
 };
