816f7398965f83cd696b0deeefbc4f97df45cabf
Suresh Selvaraj <suresh.s@netserv.local>
2018-02-20 00:37:27 +0530

    Select Offering Table UI Pagination and filter Added

--- src/scripts/select-plan-offering/controller/select-plan-offering.controller.js
+++ src/scripts/select-plan-offering/controller/select-plan-offering.controller.js
@@ -1,48 +1,54 @@
 'use strict';
 
 var module = angular.module('TT-UI-CLM.SelectPlanOffering.Controllers.SelectPlanOfferingCtrl', [
-    'TT-UI-CLM.SelectPlanOffering.Services.SelectPlanOfferingService',
-    //  'TT-UI-CLM.SelectPlanOffering.Services.SelectOfferingPlanAPIService',
+	'TT-UI-CLM.SelectPlanOffering.Services.SelectPlanOfferingService',
+	'smart-table',
+	'TT-UI.Table',
+	'ttui-table.tpl',
+	'uib/template/modal/window.html',
+	'uib/template/modal/backdrop.html',
+	'ui.bootstrap.modal'
+	//  'TT-UI-CLM.SelectPlanOffering.Services.SelectOfferingPlanAPIService',
 ])
 
-function SelectPlanOfferingCtrl($scope, $parse, selectPlanOfferingService) {
-    $scope.offeringData = [];
-    $scope.selectedVariant = {
-        code: ""
-    };
-    $scope.selectAllowance = "";
+function SelectPlanOfferingCtrl($scope, $parse, selectPlanOfferingService, $timeout, $uibModal) {
+	$scope.offeringData = [];
+	$scope.selectedVariant = {
+		code: ""
+	};
+	$scope.selectAllowance = "";
 
-    this.setInitialState = function () {
-        this.getOfferings();
-    }
+	this.setInitialState = function () {
+		this.getOfferings();
+	}
 
-    this.getOfferings = function () {
-        this.setSelectOfferings();
-        //this.setAllowanceDescription();
-    }
+	this.getOfferings = function () {
+		this.setSelectOfferings();
+		//this.setAllowanceDescription();
+	}
 
-    this.setSelectOfferings = function () {
-        var offering = $parse('masterData.response.body')($scope);
+	this.setSelectOfferings = function () {
+		var offering = $parse('masterData.response.body')($scope);
 
-        $scope.offeringData = offering.map(function (offering) {
-            return angular.extend({}, {
-                selected: false
-            }, offering);
-        });
-    }
-    this.setAllowanceDescription = function () {
-        var offering = $parse('masterData.response.body')($scope);
+		$scope.offeringData = offering.map(function (offering) {
+			return angular.extend({}, {
+				selected: false
+			}, offering);
+		});
+	}
+	this.setAllowanceDescription = function () {
+		var offering = $parse('masterData.response.body')($scope);
 
-        $scope.offeringData = offering.map(function (offer) {
-            return angular.extend({}, {
-                allowanceDescription: offer.offering.allowances.allowance[0].dedicatedAccounts.dedicatedAccount[0].description
-            }, offer);
-        });
+		$scope.offeringData = offering.map(function (offer) {
+			return angular.extend({}, {
+				allowanceDescription: offer.offering.allowances.allowance[0].dedicatedAccounts.dedicatedAccount[0].description
+			}, offer);
+		});
 
-    }
+	}
 
-    $scope.getSelectedVariant = function (selectedOffer) {
-        setSelectedVariantAllowance(selectedOffer);
+	$scope.getSelectedVariant = function (selectedOffer) {
+		setSelectedVariantAllowance(selectedOffer);
 	}
 
 	this.setSelectOfferings = function () {
@@ -51,25 +57,25 @@ function SelectPlanOfferingCtrl($scope, $parse, selectPlanOfferingService) {
 		var items = [{
 				summary: "My T 100 M comes with detfult Essential  bouquet offering, and a choice of 3 other bouquets at a diferential price to choose from (this is the offering description). With upto 500 channels and 100 GB of data.",
 				title: "inclusions Allowance",
-				id:0,
-				code:"inclusionsAllowance",
+				id: 0,
+				code: "inclusionsAllowance",
 				inclusionsAllowance: [{
 						code: 1212331,
 						item1: 'Item 1 product name',
 						item2: 'Item 2 product name'
 					},
-					{   
+					{
 						code: 122212331,
 						item1: 'Item 3 product name',
 						item2: 'Item 4 product name'
 					}
 				],
 			},
-			{id:1,	
+			{
 				summary: 'My T 200 M comes with detfult Essential  bouquet offering, and a choice of 3 other bouquets at a diferential price to choose from (this is the offering description). With upto 500 channels and 100 GB of data.',
 				title: "Equipments",
-				id:1,
-				code:"equipments",
+				id: 1,
+				code: "equipments",
 				equipments: [{
 						code: 1212331,
 						item1: 'Item 1 product name',
@@ -82,11 +88,11 @@ function SelectPlanOfferingCtrl($scope, $parse, selectPlanOfferingService) {
 					}
 				],
 			},
-			{id:2,	
+			{
 				summary: 'My T 300 M comes with detfult Essential  bouquet offering, and a choice of 3 other bouquets at a diferential price to choose from (this is the offering description). With upto 500 channels and 100 GB of data.',
 				title: "Charges",
-				id:2,
-				code:"charges",
+				id: 2,
+				code: "charges",
 				charges: [{
 						code: 1212331,
 						item1: 'Item 1 product name',
@@ -99,11 +105,11 @@ function SelectPlanOfferingCtrl($scope, $parse, selectPlanOfferingService) {
 					}
 				],
 			},
-			{id:3,	
+			{
 				summary: 'My T 100 M comes with detfult Essential  bouquet offering, and a choice of 3 other bouquets at a diferential price to choose from (this is the offering description). With upto 500 channels and 100 GB of data.',
 				title: "Contracts Penalty",
-				id:3,
-				code:"contractsPenalty",
+				id: 3,
+				code: "contractsPenalty",
 				contractsPenalty: [{
 						code: 1212331,
 						item1: 'Item 1 product name',
@@ -116,11 +122,11 @@ function SelectPlanOfferingCtrl($scope, $parse, selectPlanOfferingService) {
 					}
 				],
 			},
-			{id:4,	
+			{
 				summary: 'My T 100 M comes with detfult Essential  bouquet offering, and a choice of 3 other bouquets at a diferential price to choose from (this is the offering description). With upto 500 channels and 100 GB of data.',
 				title: "EMI Plans",
-				id:4,
-				code:"EMIPlans",
+				id: 4,
+				code: "EMIPlans",
 				EMIPlans: [{
 						code: 1212331,
 						item1: 'Item 1 product name',
@@ -143,58 +149,54 @@ function SelectPlanOfferingCtrl($scope, $parse, selectPlanOfferingService) {
 
 			}, offering);
 		});
-		
+
 	}
 
-     var setSelectedVariantAllowance = function (selectedOffer) {
-        var selectedVariant = $scope.selectedVariant.code;
-        var offeringData = $scope.offeringData;
-        $scope.offeringData.map(function (offer) {
-            if (selectedOffer.code === offer.offering.code) {
-                var allowance = $parse('allowances.allowance')(offer.offering)
-                allowance.find(function (obj) {
-                    obj.product.code === selectedVariant ? offer.allowanceDescription = obj.dedicatedAccounts.dedicatedAccount[0].description : "";
-                });
-            }
+	var setSelectedVariantAllowance = function (selectedOffer) {
+		var selectedVariant = $scope.selectedVariant.code;
+		var offeringData = $scope.offeringData;
+		$scope.offeringData.map(function (offer) {
+			if (selectedOffer.code === offer.offering.code) {
+				var allowance = $parse('allowances.allowance')(offer.offering)
+				allowance.find(function (obj) {
+					obj.product.code === selectedVariant ? offer.allowanceDescription = obj.dedicatedAccounts.dedicatedAccount[0].description : "";
+				});
+			}
 
-        });
-    }
+		});
+	}
 
-    $scope.selectOffering = function (offer) {
-        var selectedVariant = $scope.selectedVariant.code;
-        var offeringData = $scope.offeringData;
-        $scope.offeringData.map(function (offer) {
-            selectedOffer.code === offer.offering.code ? offer.selected = !offer.selected : "";
-        });
-        Spinner.inner.show();
-        // SelectOfferingPlanAPIService(req).then(function(res) {
-        //     debugger
-        //     Spinner.inner.hide();
-        // }).catch(function(err) {
-        //     Spinner.inner.hide();            
-        //     console.log(err);
-        // });
-    }
+	$scope.selectOffering = function (offer) {
+		var selectedVariant = $scope.selectedVariant.code;
+		var offeringData = $scope.offeringData;
+		$scope.offeringData.map(function (offer) {
+			selectedOffer.code === offer.offering.code ? offer.selected = !offer.selected : "";
+		});
+		Spinner.inner.show();
+		// SelectOfferingPlanAPIService(req).then(function(res) {
+		//     
+		//     Spinner.inner.hide();
+		// }).catch(function(err) {
+		//     Spinner.inner.hide();            
+		//     console.log(err);
+		// });
+	}
 	this.setInitialState();
 	$scope.getOfferingDetailsView = function () {
-		
+
 		$scope.tabs = []
 		$scope.offeringData.map(function (offerDetails, $index) {
-			
+
 			$scope.tabs = $parse('details')(offerDetails);
 		})
 	}
 
-	this.tabId = 0;
+	$scope.tabId = 0;
 
-	this.setTab = function (tabId) {
-		debugger
-		this.tabId = tabId;
-	};
+	$scope.setTab = function (tabId, $event) {
 
-	this.isSet = function (tabId) {
-		debugger
-		return this.tabId === tabId;
+		$scope.tabId = tabId;
+		$event.stopPropagation();
 	};
 
 }
@@ -202,6 +204,8 @@ function SelectPlanOfferingCtrl($scope, $parse, selectPlanOfferingService) {
 SelectPlanOfferingCtrl.$inject = [
 	'$scope',
 	'$parse',
-	'SelectPlanOfferingService'
+	'SelectPlanOfferingService',
+	'$timeout',
+	'$uibModal',
 ]
 module.controller(SelectPlanOfferingCtrl.name, SelectPlanOfferingCtrl)
